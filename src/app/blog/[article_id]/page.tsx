3e267eb4adb81ad3d4fd1c738ea0b902feb46169
Taro Morita <morita0079@gmail.com>
2023-05-17 01:46:45 +0900

    delete client component

--- src/app/blog/[article_id]/page.tsx
+++ src/app/blog/[article_id]/page.tsx
@@ -1,26 +1,41 @@
-import ContentComponent from '../components/ContentComponent'
-import { Article } from '../page'
+import { utcToZonedTime } from "date-fns-tz";
+import parse from "react-html-parser";
+import { Article } from "../page";
 
 const getArticle = async (url: string) => {
   const res = await fetch(url, {
     headers: {
-      'X-MICROCMS-API-KEY': `${process.env.X_MICROCMS_API_KEY}`,
-      'Content-Type': 'application/json'
+      "X-MICROCMS-API-KEY": `${process.env.X_MICROCMS_API_KEY}`,
+      "Content-Type": "application/json",
     },
-    cache: 'no-store'
-  })
+  });
   if (!res.ok) {
-    throw new Error('Failed to fetch data from microCMS')
+    throw new Error("Failed to fetch data from microCMS");
   }
-  const json = await res.json()
-  return json as Article
-}
+  const json = await res.json();
+  return json as Article;
+};
 const Page = async (ctx: { params: { article_id: string } }) => {
-  const articleId = ctx.params.article_id
-  const url = 'https://taroblog.microcms.io/api/v1/blog/' + articleId
+  const articleId = ctx.params.article_id;
+  const url = "https://taroblog.microcms.io/api/v1/blog/" + articleId;
 
-  const article = await getArticle(url)
-  return <ContentComponent article={article} />
-}
+  const article = await getArticle(url);
+  return (
+    <div className="justify-center">
+      <h2 className="md:hidden">{article.title}</h2>
+      <h1 className="hidden md:flex">{article.title}</h1>
+      <p>
+        投稿日:{" "}
+        {utcToZonedTime(
+          new Date(article.publishedAt),
+          "Asia/Tokyo"
+        ).toLocaleString("ja-JP")}
+      </p>
+      <div className="justify-center">
+        <div>{parse(article.body)}</div>
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
