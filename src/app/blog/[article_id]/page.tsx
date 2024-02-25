c305741a7d3691659ee3d4be5c6dc376b6e95fdf
Taro Morita <morita0079@gmail.com>
2024-02-25 12:02:15 +0900

    Add NextUIProvider and update dependencies

--- src/app/blog/[article_id]/page.tsx
+++ src/app/blog/[article_id]/page.tsx
@@ -1,40 +1,40 @@
-import { utcToZonedTime } from "date-fns-tz";
-import parse from "react-html-parser";
-import CodeBlocked from "../components/CodeBlocked";
-import { Article, getPreArticleData } from "../getPreArticleData";
-import "./blog.css";
+import { utcToZonedTime } from "date-fns-tz"
+import parse from "react-html-parser"
+import CodeBlocked from "../components/CodeBlocked"
+import { Article, getPreArticleData } from "../getPreArticleData"
+import "./blog.css"
 
 export async function generateStaticParams() {
-  const data = await getPreArticleData();
-  const articles = data.contents;
+  const data = await getPreArticleData()
+  const articles = data.contents
   const paths = articles.map((article) => {
     return {
-      article_id: article.id,
-    };
-  });
-  return [...paths];
+      article_id: article.id
+    }
+  })
+  return [...paths]
 }
 
 const getArticle = async (url: string) => {
   const res = await fetch(url, {
     headers: {
       "X-MICROCMS-API-KEY": `${process.env.X_MICROCMS_API_KEY}`,
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    next: { revalidate: 60 },
-  });
+    next: { revalidate: 60 }
+  })
   if (!res.ok) {
-    throw new Error("Failed to fetch data from microCMS");
+    throw new Error("Failed to fetch data from microCMS")
   }
-  const json = await res.json();
-  return json as Article;
-};
+  const json = await res.json()
+  return json as Article
+}
 const Page = async (ctx: { params: { article_id: string } }) => {
-  const articleId = ctx.params.article_id;
-  const url = "https://taroblog.microcms.io/api/v1/blog/" + articleId;
+  const articleId = ctx.params.article_id
+  const url = "https://taroblog.microcms.io/api/v1/blog/" + articleId
 
-  const prearticle = await getArticle(url);
-  const article = CodeBlocked(prearticle);
+  const prearticle = await getArticle(url)
+  const article = CodeBlocked(prearticle)
 
   return (
     <div className="w-full md:w-2/3">
@@ -53,7 +53,7 @@ const Page = async (ctx: { params: { article_id: string } }) => {
         {parse(article.body)}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
--- src/app/blog/pages/[id]/page.tsx
+++ src/app/blog/pages/[id]/page.tsx
@@ -1,49 +1,52 @@
-import BlogPost from "@/app/components/BlogPost";
-import Pagination from "@/app/components/Pagination";
-import { BlogData, getPreArticleData } from "../../getPreArticleData";
-import { Generator } from "@/app/components/Pagination/generater";
+import BlogPost from "@/app/components/BlogPost"
+import Pagination from "@/app/components/Pagination"
+import { BlogData, getPreArticleData } from "../../getPreArticleData"
+import { Generator } from "@/app/components/Pagination/generater"
+import { Input } from "@nextui-org/input"
+import { Button } from "@nextui-org/button"
+import React, { ChangeEvent, useState } from "react"
 
 export async function generateStaticParams() {
-  const data = await getPreArticleData();
-  const paginate = Generator(data.totalCount, data.limit);
+  const data = await getPreArticleData()
+  const paginate = Generator(data.totalCount, data.limit)
   const paths = paginate.map((p) => {
     return {
-      id: p.toString(),
-    };
-  });
-  return [...paths];
+      id: p.toString()
+    }
+  })
+  return [...paths]
 }
 
 const getOffsetBlogData = async (id: number, limit: number) => {
-  const offset = (id - 1) * limit;
+  const offset = (id - 1) * limit
   const data = await fetch(
     "https://taroblog.microcms.io/api/v1/blog?offset=" + offset,
     {
       headers: {
         "X-MICROCMS-API-KEY": `${process.env.X_MICROCMS_API_KEY}`,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
-      next: { revalidate: 60 },
+      next: { revalidate: 60 }
     }
-  );
+  )
   if (!data.ok) {
-    throw new Error("Failed to fetch data from microCMS");
+    throw new Error("Failed to fetch data from microCMS")
   }
-  const json = await data.json();
-  return json as BlogData;
-};
+  const json = await data.json()
+  return json as BlogData
+}
 
 const Page = async (ctx: { params: { id: number } }) => {
-  const preData = await getOffsetBlogData(ctx.params.id, 10);
-  const totalCount = preData.totalCount;
-  const limit = preData.limit;
-  const articles = preData.contents;
+  const preData = await getOffsetBlogData(ctx.params.id, 10)
+  const totalCount = preData.totalCount
+  const limit = preData.limit
+  const articles = preData.contents
   return (
     <div className="w-full md:w-2/3">
       <BlogPost articles={articles} />
       <Pagination totalCount={totalCount} limit={limit} />
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
